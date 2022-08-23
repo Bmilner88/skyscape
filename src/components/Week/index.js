9478458847272aff5e5825efb2521b9c27c64f5c
Ben Milner <bmilner88@gmail.com>
2022-08-23 14:58:39 -0700

    Added day of week string to 7-day

--- src/components/Week/index.js
+++ src/components/Week/index.js
@@ -1,6 +1,27 @@
 import React from "react";
 
 function Week({ week }) {
+  function dayOfWeek(num) {
+    switch (num) {
+      case 0:
+        return "Sun";
+      case 1:
+        return "Mon";
+      case 2:
+        return "Tue";
+      case 3:
+        return "Wed";
+      case 4:
+        return "Thu";
+      case 5:
+        return "Fri";
+      case 6:
+        return "Sat";
+      default:
+        return;
+    }
+  }
+
   return (
     <div className="container p-5">
       <div className="row">
@@ -9,6 +30,8 @@ function Week({ week }) {
           week.map((day) => (
             <div className="m-2 col-sm">
               <h5 className="">
+                {dayOfWeek(new Date(day.dt * 1000).getDay())}
+                <br></br>
                 {new Date(day.dt * 1000).toLocaleDateString().slice(0, 4)}
               </h5>
               <h6 className="">{`${day.weather[0].main}`}</h6>
@@ -17,7 +40,6 @@ function Week({ week }) {
                 src={`http://openweathermap.org/img/wn/${day.weather[0].icon}.png`}
                 alt="weather icon"
               />
-
               <h6>
                 {`High: ${day.temp.day.toFixed()}°`}
                 <br></br>
